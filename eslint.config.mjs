4a363fb9ba9c15245d332ad8907ce6cf471e153e
lisboasamuu <lisboasamuu@gmail.com>
2026-08-11 01:43:48 -0300

    fix: correct eslint flat configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '.wwebjs_auth/**',
+      '.wwebjs_cache/**',
+    ],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['src/**/*.ts'],
+  },
+);
